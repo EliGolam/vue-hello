f26cef857b7c43267b255e3fbfd5bf33c5cfe07e
EliGolam <elistasibusiness@gmail.com>
2022-09-12 16:34:25 +0200

    Small implementation with an editable card

--- scripts/script.js
+++ scripts/script.js
@@ -6,5 +6,36 @@ const app = new Vue ({
     el: '#root',
     data: {
         userName: '',
+        name: '',
+        age: '',
+        nationality: '',
     }
 });
+
+
+const infoEdit = document.querySelector('.infoEdit');
+const info = document.querySelector('.info');
+console.log(info, infoEdit);
+
+
+const editBtn = document.getElementById('editBtn');
+const confirmBtn = document.getElementById('confirmBtn');
+
+
+editBtn.addEventListener('click', edit);
+confirmBtn.addEventListener('click', confirm);
+
+
+function confirm() {
+    console.log('Confirmed');
+    infoEdit.classList.remove('active');
+    info.classList.add('active');
+}
+
+function edit() {
+    console.log('Edit');
+    infoEdit.classList.add('active');
+    info.classList.remove('active');
+}
+
+
